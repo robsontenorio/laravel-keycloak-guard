09578954f3c34985d2c4bafe565d73a3260d27b8
Falkenberg, Dennis <D.Falkenberg@wertgarantie.com>
2022-04-21 09:56:34 +0200

    add build key for server key

--- src/Token.php
+++ src/Token.php
@@ -24,6 +24,7 @@ class Token
 
         if (!empty($keyCloakServer)) {
             $publicKey = self::getPublicFromKeyCloak($keyCloakServer);
+            $publicKey = self::buildPublicKey($publicKey);
         }
 
         if (empty($publicKey)) {
