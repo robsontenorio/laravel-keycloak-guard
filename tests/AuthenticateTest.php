672140ba17846364ff07ead3095602fac962c535
Robson Tenório <robson.tenorio@tjdft.jus.br>
2022-08-31 23:10:35 +0000

    WIP

--- tests/AuthenticateTest.php
+++ tests/AuthenticateTest.php
@@ -18,8 +18,7 @@ class AuthenticateTest extends TestCase
         parent::setUp();
     }
 
-    /** @test */
-    public function it_authenticates_the_user_when_request_any_endpoint_with_token()
+    public function test_authenticates_the_user_when_request_any_endpoint_with_token()
     {
         $this->withKeycloakToken()->json('GET', '/foo/secret');
 
@@ -30,16 +29,14 @@ class AuthenticateTest extends TestCase
         $this->assertEquals($this->user->username, Auth::user()->username);
     }
 
-    /** @test */
-    public function it_forbiden_when_request_a_protected_endpoint_without_token()
+    public function test_forbiden_when_request_a_protected_endpoint_without_token()
     {
         $response = $this->json('GET', '/foo/secret');
 
         $response->assertStatus(401);
     }
 
-    /** @test */
-    public function it_throws_a_exception_when_user_is_not_found()
+    public function test_throws_a_exception_when_user_is_not_found()
     {
         $this->expectException(UserNotFoundException::class);
         $this->withoutExceptionHandling();
@@ -51,8 +48,7 @@ class AuthenticateTest extends TestCase
         $this->withKeycloakToken()->json('GET', '/foo/secret');
     }
 
-    /** @test */
-    public function it_throws_a_exception_when_resource_access_is_not_allowed_by_api()
+    public function test_throws_a_exception_when_resource_access_is_not_allowed_by_api()
     {
         $this->expectException(ResourceAccessNotAllowedException::class);
         $this->withoutExceptionHandling();
@@ -64,8 +60,7 @@ class AuthenticateTest extends TestCase
         $this->withKeycloakToken()->json('GET', '/foo/secret');
     }
 
-    /** @test */
-    public function it_appends_token_to_the_user()
+    public function test_appends_token_to_the_user()
     {
         config(['keycloak.append_decoded_token' => true]);
 
@@ -74,8 +69,7 @@ class AuthenticateTest extends TestCase
         $this->assertNotNull(Auth::user()->token);
     }
 
-    /** @test */
-    public function it_does_not_appends_token_to_the_user()
+    public function test_does_not_appends_token_to_the_user()
     {
         config(['keycloak.append_decoded_token' => false]);
 
@@ -84,8 +78,7 @@ class AuthenticateTest extends TestCase
         $this->assertNull(Auth::user()->token);
     }
 
-    /** @test */
-    public function it_does_not_load_user_from_database()
+    public function test_does_not_load_user_from_database()
     {
         config(['keycloak.load_user_from_database' => false]);
 
@@ -94,8 +87,7 @@ class AuthenticateTest extends TestCase
         $this->assertCount(0, Auth::user()->getAttributes());
     }
 
-    /** @test */
-    public function it_does_not_load_user_from_database_but_appends_decoded_token()
+    public function test_does_not_load_user_from_database_but_appends_decoded_token()
     {
         config(['keycloak.load_user_from_database' => false]);
         config(['keycloak.append_decoded_token' => true]);
@@ -105,8 +97,7 @@ class AuthenticateTest extends TestCase
         $this->assertArrayHasKey('token', Auth::user()->toArray());
     }
 
-    /** @test */
-    public function it_check_user_has_role_in_resource()
+    public function test_check_user_has_role_in_resource()
     {
         $this->buildCustomToken([
             'resource_access' => [
@@ -129,8 +120,7 @@ class AuthenticateTest extends TestCase
         $this->assertTrue(Auth::hasRole('myapp-backend', 'myapp-backend-role1'));
     }
 
-    /** @test */
-    public function it_check_user_no_has_role_in_resource()
+    public function test_check_user_no_has_role_in_resource()
     {
         $this->buildCustomToken([
             'resource_access' => [
@@ -153,8 +143,7 @@ class AuthenticateTest extends TestCase
         $this->assertFalse(Auth::hasRole('myapp-backend', 'myapp-backend-role3'));
     }
 
-    /** @test */
-    public function it_prevent_cross_roles_resources()
+    public function test_prevent_cross_roles_resources()
     {
         $this->buildCustomToken([
             'resource_access' => [
@@ -177,8 +166,7 @@ class AuthenticateTest extends TestCase
         $this->assertFalse(Auth::hasRole('myapp-backend', 'myapp-frontend-role1'));
     }
 
-      /** @test */
-      public function custom_user_retrieve_method()
+      public function teststom_user_retrieve_method()
       {
           config(['keycloak.user_provider_custom_retrieve_method' => 'custom_retrieve']);
 
@@ -190,8 +178,7 @@ class AuthenticateTest extends TestCase
           $this->assertTrue(Auth::user()->customRetrieve);
       }
 
-      /** @test */
-    public function it_throws_a_exception_with_invalid_iat()
+    public function test_throws_a_exception_with_invalid_iat()
     {
         $this->expectException(TokenException::class);
         $this->withoutExceptionHandling();
@@ -205,8 +192,7 @@ class AuthenticateTest extends TestCase
         $this->withKeycloakToken()->json('GET', '/foo/secret');
     }
 
-      /** @test */
-      public function it_works_with_leeway()
+      public function test_works_with_leeway()
       {
           // Allows up to 60 seconds ahead in the  future
           config(['keycloak.leeway' => 60]);
