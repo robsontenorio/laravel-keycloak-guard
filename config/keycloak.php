9d5114e8396026255b78ee439b9cfdc5404e6d2c
Daniel Feneck <feneck@hotmail.com>
2024-10-14 03:22:40 +1000

    Fixed an issue with the configuration variable ‘user_provider_custom_retrieve_method’ being manually set to null rather than appropriately loading from the ENV. Additionally added a slight bit more detail on how to use it in the readme.

--- config/keycloak.php
+++ config/keycloak.php
@@ -7,7 +7,7 @@ return [
 
     'load_user_from_database' => env('KEYCLOAK_LOAD_USER_FROM_DATABASE', true),
 
-    'user_provider_custom_retrieve_method' => null,
+    'user_provider_custom_retrieve_method' => env('KEYCLOAK_USER_PROVIDER_CUSTOM_RETRIEVE_METHOD', null),
 
     'user_provider_credential' => env('KEYCLOAK_USER_PROVIDER_CREDENTIAL', 'username'),
 
