81a764ba68f37e42b8f85f8b70a5e6363e5273c0
lyrasteph <42931951+lyrasteph@users.noreply.github.com>
2024-12-02 14:02:07 +0100

    Update KeycloakGuard.php

--- src/KeycloakGuard.php
+++ src/KeycloakGuard.php
@@ -201,17 +201,11 @@ class KeycloakGuard implements Guard
      */
     public function hasRole($resource, $role)
     {
-        $token_resource_access = (array)$this->decodedToken->resource_access;
-
-        if (array_key_exists($resource, $token_resource_access)) {
-            $token_resource_values = (array)$token_resource_access[$resource];
-
-            if (array_key_exists('roles', $token_resource_values) &&
-              in_array($role, $token_resource_values['roles'])) {
-                return true;
-            }
+        $token_roles = (array)$this->decodedToken->userData->userroles;
+        if (str_contains($role, $token_roles)) {
+            return true;
         }
-
+        
         return false;
     }
     
