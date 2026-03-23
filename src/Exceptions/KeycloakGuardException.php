011cfa04b2975d2744820bd878e2003ff1fe0aae
Robson Tenório <rrtenorio@gmail.com>
2026-03-23 18:56:15 -0300

    Fix - Actions and some changes (#143)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * fix code style
    
    * WIP
    
    * fix code style
    
    * WIP
    
    * WIP
    
    * fix code style

--- src/Exceptions/KeycloakGuardException.php
+++ src/Exceptions/KeycloakGuardException.php
@@ -8,6 +8,6 @@ class KeycloakGuardException extends UnexpectedValueException
 {
     public function __construct(string $message)
     {
-        $this->message = "[Keycloak Guard] {$message}";
+        parent::__construct("[Keycloak Guard] {$message}");
     }
 }
